0943bd9870752b347c1762267174fe7337674424
Lorence Lim <jlorencelim@gmail.com>
2019-02-26 17:35:03 +0800

    Remove size in heroku formation

--- {{cookiecutter.snake_case_slug}}/variables.tf
+++ {{cookiecutter.snake_case_slug}}/variables.tf
@@ -13,7 +13,7 @@ variable "custom_domain" {
 variable "dyno_size" {
   description = "Size of Heroku dynos"
   type = "string"
-  default = "standard-1x"
+  default = "standard-1X"
 }
 
 variable "repo_url" {
